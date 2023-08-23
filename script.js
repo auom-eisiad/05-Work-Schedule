00f8ade354a300da593cfe04b0ceabcf489286e8
auom.eisiad <daisiemouaa@gmail.com>
2023-08-23 15:45:19 -0400

    added detailed comments

--- script.js
+++ script.js
@@ -6,19 +6,29 @@ function displayDay() {
 
 displayDay();
 
+// For each hour passed, the time block will change color whether the item is past, present, or future
 function eachHour() {
+
+  // Go through and change for each time block
   $(".time-block").each(function() {
+
+    // Ensure the time block id is read in integar
     var scheduledHour = parseInt($(this).attr("id"));
 
     // Ensure the current hour is read in integar
     var currentHour = parseInt(dayjs().hour());
 
+      // If the scheduled hour is less than the current hour, color change to grey
       if (scheduledHour < currentHour) {
         $(this).addClass("past");
       }
+
+      // Else if the scheduled hour is equal to the current hour, color change to red
       else if (scheduledHour === currentHour) {
           $(this).addClass("present");
       }
+
+      // Else, color change to green
       else {
           $(this).addClass("future");
       }
@@ -27,18 +37,25 @@ function eachHour() {
 
 eachHour();
 
+// Save button is clickable and able to store user's event inputs
 $(".saveBtn").on("click", function (event) {
   event.preventDefault();
 
+  // Take the save button's sibling, description, value
   const scheduledEvent = $(this).siblings(".description").val();
+
+  // Take the save button's parent, id
   const time = $(this).parent().attr("id");
 
+  // Store the time and scheduled events
   localStorage.setItem(time, scheduledEvent);
 });
 
 // Loop through the time blocks and get their saved scheduled events
 for (let i = 9; i < 18; i++) {
   const scheduledEvent = localStorage.getItem(i);
+
+  // $(`#9 .description`).val(scheduledEvent);
   $(`#${i} .description`).val(scheduledEvent);
 }
 
@@ -64,6 +81,7 @@ var quotes = [
   },
 ];
 
+// Manipulate the DOM to load and parse object
 document.addEventListener("DOMContentLoaded", function() {
   // Generate a random index
   var randomIndex = Math.floor(Math.random() * quotes.length);
